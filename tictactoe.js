461ad348bdb3fbad71b2b735d018e7f6452c8c51
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-10-07 17:15:31 +1100

    Add rudimentary board object

---
+++ tictactoe.js
@@ -0,0 +1,60 @@
+"use strict";
+
+/**
+ * Tests whether something is between (INCLUSIVE) two limits.
+ * Use Infinity/-Infinity for unbounded limits.
+ * @param {Number} x 
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Boolean}
+ */
+const between = function(x, min, max) {
+  if ((x >= min) & (x <= max)) {
+    return true
+  }
+  return false
+}
+
+const board = (() => {
+  // needs to tell you what is needed to draw it
+  const xMin = 0;
+  const xMax = 2;
+  const yMin = 0;
+  const yMax = 2;
+  
+  const spaces = {};
+  // spaces["1,1"] = "x";
+
+  // questionable value
+  const withinBorders = function(x, y){
+    if ((between(x, xMin, xMax)) & between(y, yMin, yMax)) {
+      return true;
+    }
+    return false;
+  }
+
+  const clearBoard = function() {
+    console.log("Board-clear not implemented")
+  }
+
+  // Thinking about infinite boards is bogging me down, so
+  // I'll start with creating a 3x3 flat board
+  const newBoard = function() {
+    clearBoard()
+    for (let x = xMin; x <= xMax; x++) {
+      for (let y = yMin; y <= yMax; y++) {
+        spaces[`${x},${y}`] = null;
+      }
+    }
+  }
+
+  const setMove = function(x, y, value) {
+
+  }
+
+  return {
+    clearBoard,
+    newBoard,
+    setMove,
+    spaces};
+})();
