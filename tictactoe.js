4448184fd176fd86a2afe7f2bc7cb880e3698f90
J Walsh <qc@armoraustralia.com>
2023-10-10 10:46:13 +1100

    Solve bug and add explanatory comments.
    You can't expose primitives (e.g. for state) in a module.
    They have to be accessed by a member function, because
    only functions will have the required scope to get the
    actual value.

--- tictactoe.js
+++ tictactoe.js
@@ -4,14 +4,31 @@ const doc = document;
 const playArea = doc.querySelector(".board");
 const gameStatus = doc.querySelector(".status")
 
+playArea.addEventListener("click", (e) => { // relatively confident event handlers are safe
+  console.log(`Clicked on thing!`)
+  controller.handle();
+})
+
+// =========== bug should exist below the line ============
+
 const controller = (() => {
-  let status = "Controller online..."
+  let _status = "Controller online..." // confirmed this line only runs once
+  const status = function() {
+    return _status;
+  }
 
   const handle = function () {
-    status = "Your turn:";
+    _status = "Your turn:";
     surface.drawBoard();
-    console.log(status);
+    console.log(status());
   }
+
+  // The true cause of the bug is here, where members of the function
+  // context are actually exposed in the returned object. If you return
+  // function references, that's fine, but if you return one of the PRIMITIVE
+  // properties, it gets immediately resolved, and that's all that the outside
+  // will ever see.
+  // Different variable, same name.
 
   return {
     handle,
@@ -19,21 +36,16 @@ const controller = (() => {
   };
 })();
 
-const surface = ((el) => {
-  el.addEventListener("click", (e) => {
-    console.log(`Clicked on thing!`)
-    controller.handle();
-  })
-
+const surface = (() => {
   const drawBoard = function() {
-    console.log(controller.status);
-    gameStatus.textContent = controller.status;
+    console.log(controller.status());
+    gameStatus.textContent = controller.status();
+    // The "visible" part of the bug is that trying to access
+    // controller.status from the outside (controller._status now)
+    // will ONLY see he original value.
   }
 
   return {
     drawBoard
   };
-})(playArea);
-
-// setup
-surface.drawBoard();
+})();
